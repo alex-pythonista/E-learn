87d1de221611d2eb20192bc12a4a12a34dec6551
Shaon Indrajit Barua Alex <68987775+alex-pythonista@users.noreply.github.com>
2020-10-14 20:18:53 +0600

    coomit

--- eSchool/eSchool/settings.py
+++ eSchool/eSchool/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'eSchool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eSchool_db',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
